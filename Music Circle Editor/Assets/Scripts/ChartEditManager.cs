dd3555d6d8d963a4e2209a4f3784ebce5e1378b2
SeunghoeLee <shlee259p@kaist.ac.kr>
2019-05-17 22:05:16 +0900

    Initial Commit

---
+++ Music Circle Editor/Assets/Scripts/ChartEditManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ChartEditManager : MonoBehaviour
+{
+    public string fileName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextAsset chart = Resources.Load("Charts/" + fileName, typeof(TextAsset)) as TextAsset;
+        StringReader sr = new StringReader(chart.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
